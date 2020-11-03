0a68efb9fa5fae8785cd352aa6e9a5433649d029
shamaz332 <shamazsaeed332@gmail.com>
2020-11-03 20:39:02 +0500

    data fetched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
-import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import React from 'react'
+import { graphql } from "gatsby"
+export default ({data}) => {
+  return (
+    <div>
+    <div>{data.allContentfulBlogPost.edges[0].node.title}</div>
+    <div>{data.allContentfulBlogPost.edges[0].node.publicationDate}</div>
+    <div>{data.allContentfulBlogPost.edges[0].node.content.content}</div>
+  </div>
+  )
 }
+
+
+
+
+export const query = graphql`
+
+query{
+  allContentfulBlogPost {
+    edges {
+      node {
+        title
+        publicationDate
+        content{
+          content
+        }
+      }
+      
+    }
+  }
+}
+
+
+
+
+
+`
